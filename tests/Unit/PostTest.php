dd49f0639f6b96960fe029fcd8af52f6001d2873
yxj0312 <yanxiajun@hotmail.com>
2020-01-31 19:40:41 +0100

    Add first test of post

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -2,13 +2,18 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    use RefreshDatabase;
+    
     /** @test */
-    function name()
+    function a_post_has_many_comments()
     {
-        
+        $post = $this->create(Post::class);
+        dd($post);
     }
 }
